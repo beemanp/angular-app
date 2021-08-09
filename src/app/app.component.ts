dc4f4402f5a1555abdf8e643f45d252993e44953
beemanp <sattawatsmile@gmail.com>
2021-08-09 13:21:00 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-app';
+  name = 'Sattawat';
+
+  ShowConsole(){
+    console.log("MMM :",this.name);
+  }
+  ChangeName(name:string){
+this.name = name;
+  }
+
+  value = 0;
+  Cal(value:string){
+    this.value = (Number(value)*3)/4
+  }
 }
